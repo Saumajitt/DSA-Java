9513ff87e7439a02f76332bf576786799095a002
Saumajit Malakar <saumajit15sam@gmail.com>
2025-10-17 14:01:49 +0530

    25- queue chapter initial

---
+++ Java Programming Language/Recorded/25_Queue/Lectures/QueueUsingArray.java
@@ -0,0 +1,63 @@
+public class QueueUsingArray {
+    static class Queue{
+        static int[] arr;
+        static int size;
+        static int rear;
+
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1;   // [0,1,2,3,4] if rear is at index before first element then queue is empty
+        }
+
+        //add 
+        public static void add(int data) {
+            if(rear == size - 1){
+                System.out.println("Queue is full");
+            } else {
+                rear++;
+                arr[rear] = data;
+            }
+        }
+
+
+        public static int delete(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            for(int i = 0; i<rear; i++){
+                arr[i] = arr[i+1];
+            }
+            rear--;
+            return front;
+        }
+
+        static int peek(){
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        while(!queue.isEmpty()){
+            System.out.println(queue.peek());
+            queue.delete();
+        } //fifo
+    }
+}
